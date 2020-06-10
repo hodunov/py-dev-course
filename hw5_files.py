d754316e2f59d2dc87febee74fcbc83d644484b8
artgo_HP <art.godunov@outlook.com>
2020-06-10 15:49:00 +0300

    finally got their hands on the last task through class.

--- hw5_files.py
+++ hw5_files.py
@@ -58,3 +58,63 @@ def align_right(file_in, file_out):
 # без повторений из первого файла. Для каждого ip укажите количество посещений,
 # наиболее популярный день недели. Последней строкой в файле
 # добавьте наиболее популярный отрезок времени в сутках длиной один час в целом для сайта.
+
+
+class IpStatistic(object):
+    """
+    This class accepts a log file with the weekly visit statistics and works with it.
+    """
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def read_file(self):
+        with open(self.file_name) as file:
+            data = [x.split() for x in file.read().split('\n')]
+            return data
+
+    def unrepeated_IPs(self):
+        """
+        ip list without repeating
+        :return: string in the format =  IP ||| visits number
+        """
+        ips = [x[0] for x in self.read_file()]
+        result_str = ""
+        for ip in set(ips):
+            result_str += f"{ip} |||  {ips.count(ip)} \n"
+        return result_str
+
+    def common_HOD(self):
+        """
+        The method determines the most popular period
+        of time per day with a length of one hour for the site as a whole.
+        :return: hour
+        """
+        hours = [x[1].split(':')[0] for x in self.read_file()]
+        result = max(hours, key=hours.count)
+        return result
+
+    def common_DOW(self):
+        """
+        method determines the most popular day of the week
+        :return: day of the week in string
+        """
+        days = [x[2] for x in self.read_file()]
+        result = max(days, key=days.count)
+        return result
+
+    def write_file(self, file_name):
+        """
+        The method creates a new text file in
+        which it writes the result of all the above methods.
+        :param file_name: file name in which the result will be saved
+        :return: resulting file
+        """
+        all_data = f"{self.unrepeated_IPs()}*** ***\n"\
+                   f"Common Hour of the Day is {self.common_DOW()}\n" \
+                   f"Common Hour of the Day is {self.common_HOD()}\n"
+        with open(file_name, 'a') as file:
+            file.write(all_data)
+
+
+IpStatistic("example.txt").write_file("result.txt")
